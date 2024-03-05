74a48c1154d84dbd3ee2930357fc97113c2be13b
Mona <mona@kb-bioinfo01.cancer.dk>
2024-03-05 14:26:20 +0100

    updated run all bash script

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 
 # uncomment and modify as necessary to set the location of the renv
 # root directory. Default is $HOME/.cache/R/renv
-export RENV_PATHS_ROOT=/data/user/mona/scratch/renv
+# export RENV_PATHS_ROOT=/data/user/mona/scratch/renv
 
 # run all analyses
 Rscript 00_init.R
