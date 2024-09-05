fa0c8c1cb4066218227e728e3896e1df54868487
subhayan-98 <s233403@dtu.dk>
2024-09-05 13:22:13 +0200

    added scripts for DMA function

---
+++ lung/scripts/13_filtering_nonGMA.R
@@ -0,0 +1,26 @@
+## This script filters for patient barcodes containing methylation, but no mutations
+
+## Load libraries
+library(tidyverse)
+library(stringr)
+
+## Load data------------
+load("lung/data/mutations_subset.rda")
+load("lung/results/DMA/Oncogenic_mediators_mutation_summary.rda")
+load("lung/results/DMA/DEG_Mutations_Annotations.rda")
+
+## Fetch names of driver genes predicted by DMA----------- 
+DMA_Driver_genes <- Oncogenic_mediators_mutation_summary %>% 
+  filter(CScape_Driver >= 1 ) %>% 
+  select(Hugo_Symbol)
+
+## Fetch patient barcodes containing these driver genes from the GMA dataset-----------
+DMA_within_GMA <- DEG_Mutations_Annotations %>% 
+  filter(Hugo_Symbol %in% DMA_Driver_genes$Hugo_Symbol) %>% 
+  mutate(Tumor_Sample_Barcode_short = substr(x = Tumor_Sample_Barcode, start = 1, stop = 15))
+
+## Check if any patient sample contains driver genes predicted by GMA but is missing from the DMA_within_GMA barcodes--------- 
+all_mutations_subset %>%
+  filter(Tumor_Sample_Barcode %in% DMA_within_GMA$Tumor_Sample_Barcode_short)
+  
+
---
+++ lung/scripts/14_GMA_DMA_overrepresentation_venn.R
@@ -0,0 +1,107 @@
+## Load libraries -----------
+library(Moonlight2R)
+library(enrichR)
+library(tidyverse)
+library(patchwork)
+library(RColorBrewer)
+library(VennDiagram)
+
+## Source functions script  --------------------------
+source("99_functions.R")
+
+## Load data ---------------
+load("lung/results/LUAD_dataEDA.rda")
+load("lung/results/LUAD_dataDMA.rda")
+
+# Get driver genes
+GMA_driver_genes <- Reduce(c, data_EDA)
+DMA_driver_genes <- Reduce(c, data_DMA)
+
+# Wrangle the two gene lists into one list
+gene_lists <- list(GMA_driver_genes,DMA_driver_genes)
+names(gene_lists) <- c('GMA_driver','DMA_driver')
+
+# Separate GMA TSGs and OCGs into lists
+GMA_TSG <- data_EDA$TSG
+GMA_OCG <- data_EDA$OCG
+DMA_TSG <- data_DMA$TSG
+DMA_OCG <- data_DMA$OCG
+
+## Analyze data ---------
+
+## Venn diagrams --------
+
+# Define colors to use in Venn diagram
+venn.cols <- c("#40E0D0", "#C8A2C8")
+
+# Make diagrams for comparison of TSGs, OCGs and all genes between the two functions
+gene_list <- list(GMA_TSG, DMA_TSG)
+venn.diagram(gene_list, filename = "lung/results/14_LUAD_venn_GMA_DMA_TSG.png", 
+             category.names = c("GMA",  "DMA"), output = TRUE, 
+             main = "Count of TSGs predicted by GMA and DMA respectively", 
+             main.cex = 1.2, main.fontface = "bold", 
+             main.fontfamily = "sans", lwd = 2, 
+             lty = "blank", fill = venn.cols, 
+             cex = 2, fontface = "bold", 
+             fontfamily = "sans", cat.cex = 1.5, 
+             cat.fontface = "bold", cat.fontfamily = "sans",
+             cat.dist = c(0.00005, 0.00005),
+             cat.pos = c( 200, -200))
+
+gene_list <- list(GMA_OCG, DMA_OCG)
+venn.diagram(gene_list, filename = "lung/results/14_LUAD_venn_GMA_DMA_OCG.png", 
+             category.names = c("GMA",  "DMA"), output = TRUE, 
+             main = "Count of OCGs predicted by GMA and DMA respectively", 
+             main.cex = 1.2, main.fontface = "bold", 
+             main.fontfamily = "sans", lwd = 2, 
+             lty = "blank", fill = venn.cols, 
+             cex = 2, fontface = "bold", 
+             fontfamily = "sans", cat.cex = 1.5, 
+             cat.fontface = "bold", cat.fontfamily = "sans",
+             cat.dist = c(0.00005, 0.00005),
+             cat.pos = c( 200, -200), ext.text = FALSE)
+
+gene_list <- list(GMA_driver_genes, DMA_driver_genes)
+venn.diagram(gene_list, filename = "lung/results/14_LUAD_venn_GMA_DMA_all.png", 
+             category.names = c("GMA",  "DMA"), output = TRUE, 
+             main = "Count of driver genes predicted by GMA and DMA respectively", 
+             main.cex = 1.1, main.fontface = "bold", 
+             main.fontfamily = "sans", lwd = 2, 
+             lty = "blank", fill = venn.cols, 
+             cex = 2, fontface = "bold", 
+             fontfamily = "sans", cat.cex = 1.5, 
+             cat.fontface = "bold", cat.fontfamily = "sans",
+             cat.dist = c(0.00005, 0.00005),
+             cat.pos = c( 200, -200))
+
+
+## Perform enrichment analysis -----------
+
+# Load databases for enrichment analysis
+dbs <- c("MSigDB_Hallmark_2020")
+
+# Perform enrichment analysis on each of the gene lists
+enrich_analysis <- map(gene_lists, function(x) { 
+  enrichr(genes = x, databases = dbs)
+})
+
+## Visualize data -----------
+
+# Visualize enrichment analysis results of MSigDB Hallmark terms of GMA driver genes
+gma_dr <- goplot(data = enrich_analysis$GMA_driver$MSigDB_Hallmark_2020, 
+                title = "MSigDB Hallmark 2020 - GMA driver genes", 
+                top = 10)
+
+dma_dr <- goplot(data = enrich_analysis$DMA_driver$MSigDB_Hallmark_2020, 
+                  title = "MSigDB Hallmark 2020 - DMA driver genes", 
+                  top = 10)
+
+# Visualize enrichment analysis of all gene lists side by side
+all_dr <- gma_dr + dma_dr
+
+# Save side by side enrichment plot of MSigDB hallmark terms
+ggsave(filename = "14_LUAD_GMA_vs_DMA.pdf",
+       plot = all_dr,
+       path = "lung/results/",
+       width = 12,
+       height = 8)
